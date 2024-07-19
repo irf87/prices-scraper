f2432fbec084a5f524361b33e33666b905ac3ed8
irf87 <irf87@hotmail.com>
2024-07-19 13:17:17 -0600

    feat: add manual sync

--- infrastructure/api/getPage.js
+++ infrastructure/api/getPage.js
@@ -1,17 +1,34 @@
+require('dotenv').config();
 const axios = require('axios');
+const { printDate } = require('../../utils/date/printDate');
+
+const DEBUG	= process.env._IS_DEBUG;
+const isDebug = DEBUG === 'true'
 
 const fetch = async (url) => {
   let dataResp;
   let errorResp;
   try {
     const { data, error } = await axios(url);
+    if (isDebug) {
+      onsole.log(`\nfetch start at ${printDate()}`);
+      console.log(`get:url ${url}`);
+    }
     dataResp = data;
+    if (isDebug) {
+      console.log(`\nget:error ${error}`);
+    }
     errorResp = error;
 
   } catch(err) {
+    if (isDebug) {
+      console.log(`\nget:error ${error}`);
+    }
     errorResp = err;
   }
-
+  if (isDebug) {
+    console.log(`fetch finish at ${printDate()}`);
+  }
   return {
     data: dataResp,
     error: errorResp,
